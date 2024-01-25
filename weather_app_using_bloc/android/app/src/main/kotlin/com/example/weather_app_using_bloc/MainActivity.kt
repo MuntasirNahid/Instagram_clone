55c614e5725e16925226a58ff5b5bf85edf9fbc6
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2024-01-25 21:49:28 +0600

    Delete weather_app_using_bloc directory

--- weather_app_using_bloc/android/app/src/main/kotlin/com/example/weather_app_using_bloc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weather_app_using_bloc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- weather_app_using_bloc/lib/bloc/weather_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'weather_bloc.dart';
-
-@immutable
-abstract class WeatherEvent {}
-
-final class WeatherFetched extends WeatherEvent {}
